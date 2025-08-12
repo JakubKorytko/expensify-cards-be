5238bd3028b8580a4faab5a54fc1c4c90a185644
Jakub Korytko <jakub@korytko.me>
2025-08-12 20:47:31 +0200

    Update endpoints

--- router.js
+++ router.js
@@ -8,22 +8,26 @@ const storage = {
 
 router.post('/key', function(req, res, next) {
     if (storage.key) {
-        res.status(401).send("Key error");
+        console.log("Key already exists");
+        res.status(401).send("Key exists");
         return;
     }
 
     const {key} = req.body;
 
     if (!key) {
-        res.status(401).send("Key error");
+        res.status(401).send("Key is not present in body");
         return;
     }
+
+    console.log("Received key", key);
 
     storage.key = key;
     res.send("OK");
 });
 
 router.get('/key', function(req, res, next) {
+    console.log("Client requested key", storage.key);
     res.json({
         key: storage.key
     })
@@ -31,10 +35,14 @@ router.get('/key', function(req, res, next) {
 
 router.delete('/key', function(req, res, next) {
     storage.key = undefined;
+    console.log('Removed key');
+    res.send("OK");
 })
 
 router.get('/token', function(req, res, next) {
     const token = ed.etc.randomBytes(8);
+
+    console.log("Token generated", ed.etc.bytesToHex(token), "and sent to client")
 
     res.json({
         bytes: token,
@@ -48,12 +56,19 @@ router.post('/verify', async function(req, res, next) {
         signature
     } = req.body;
 
+    console.log("Received token", token, "with signature", signature);
+
     if (!token || !signature || !storage.key) {
+        console.log("Invalid signature");
         res.send(false);
         return;
     }
 
-    res.send(await ed.verifyAsync(signature, token, storage.key));
+    const verificationResult = await ed.verifyAsync(signature, token, storage.key);
+
+    console.log("Verification result", verificationResult, "sent to client");
+
+    res.send(verificationResult);
 });
 
 router.use(function(req, res, next) {
