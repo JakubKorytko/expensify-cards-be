828b362c198970aaa2a0f55300e1f2ae1df2ac09
Jakub Korytko <jakub@korytko.me>
2025-08-21 13:54:03 +0200

    Clean up the code

--- router.js
+++ router.js
@@ -1,112 +1,58 @@
-const ed = require('@noble/ed25519');
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const {sha512} = require("@noble/hashes/sha2");
-ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
+const {generateSixDigitNumber, STORAGE, Logger, ed, USER_EMAIL, isChallengeValid} = require("./utils");
 
-const USER_EMAIL = "user@example.com";
-
-const STORAGE  = {
-    publicKeys: {},
-    validateCodes: {},
-    challenges: {}
-}
-
-const DISABLE_LOGGER = false;
-
-const Logger = {
-    e: (...args) => !DISABLE_LOGGER && console.error(...args) ? args : undefined,
-    w: (...args) => !DISABLE_LOGGER && console.warn(...args) ? args : undefined,
-    m: (...args) => !DISABLE_LOGGER && console.log(...args) ? args : undefined,
-};
-
-function generateSixDigitNumber() {
-    return Math.floor(Math.random() * 900000) + 100000;
-}
-
-const getNonce = (signature, key) => {
-    const nonceKeys = Object.keys(STORAGE.challenges);
-
-    const nonce = nonceKeys.find(nonceKey => {
-        Logger.m("Verifying signature", signature, "for nonce", nonceKey, "with key", key);
-        let verifyResult;
-        try {
-            verifyResult = ed.verify(signature, new TextEncoder().encode(nonceKey), key);
-        } catch (e) {
-            Logger.e(e);
-        }
-        Logger.m("Verification", verifyResult ? "successful" : "failed");
-        return verifyResult;
-    })
-    if (!nonce) {
-        return null;
-    }
-
-    return STORAGE.challenges[nonce];
-}
-
-const isTokenValid = function (signedJWTString, secretOrPublicKey, options) {
-    try {
-        const {nonce, expires} = getNonce(signedJWTString, secretOrPublicKey) ?? {};
-
-        if (!nonce || !expires) {
-            return false;
-        }
-
-        delete STORAGE.challenges[nonce];
-        const nonceExpired = expires < Date.now();
-
-        if (nonceExpired) {
-            Logger.m("Challenge expired, removed from storage")
-        } else {
-            Logger.m("Challenge success, removed from storage")
-        }
-
-        return !nonceExpired;
-    } catch (e) {
-        return false;
-    }
-}
 
 router.post("/resend_validate_code", (req, res) => {
+    Logger.m("Generating new validation code")
+
     const {email} = req.body ?? {};
     if (!email) {
         return res.status(401).send()
     }
+
     const randomCode = generateSixDigitNumber();
+
     STORAGE.validateCodes[email] ??= [];
     STORAGE.validateCodes[email].push(randomCode);
-    console.log("Generated new validation code:", randomCode, "for email", email);
+
+    Logger.m("Generated new validation code:", randomCode, "for email", email);
+
     res.status(200).send()
 })
 
 router.get("/request_biometric_challenge", (req, res) => {
-    Logger.m("Requested biometric challenge")
+    Logger.m("Requested biometric challenge");
+
     if (!STORAGE.publicKeys[USER_EMAIL]) {
         return res.status(401).send(Logger.w("Registration required"))
     }
+
     const nonce = ed.etc.bytesToHex(ed.etc.randomBytes(16));
     const expirationDate = Date.now() + 10 * 1000 * 60; // 10 minutes
-    const token = {
+
+    const challenge = {
         nonce,
         expires: expirationDate,
     }
 
-    const tokenStr = jwt.sign(token, ed.etc.bytesToHex(ed.utils.randomPrivateKey()));
-    STORAGE.challenges[tokenStr] = token;
+    const challengeJWT = jwt.sign(challenge, ed.etc.bytesToHex(ed.utils.randomPrivateKey()));
+    STORAGE.challenges[challengeJWT] = challenge;
 
     setTimeout(() => {
-        delete STORAGE.challenges[token];
+        Logger.m(`Challenge ${challengeJWT} expired, removed from storage`)
+        delete STORAGE.challenges[challengeJWT];
     }, 10 * 1000 * 60)
 
-    Logger.m("Challenge", tokenStr, "sent to the client");
+    Logger.m("Challenge", challengeJWT, "sent to the client");
+
     res.status(200).send({
-        challenge: tokenStr
+        challenge: challengeJWT
     })
 })
 
-router.post("/register_biometrics", (req, res, next) => {
+router.post("/register_biometrics", (req, res) => {
     const {publicKey, validateCode} = req.body ?? {};
     const validateCodes = STORAGE.validateCodes[USER_EMAIL] ?? [];
 
@@ -126,6 +72,7 @@ router.post("/register_biometrics", (req, res, next) => {
 
     if (validateCode && !STORAGE.publicKeys[USER_EMAIL]?.length) {
         const isValidateCodeCorrect = validateCodes.at(-1) === validateCode;
+
         if (!isValidateCodeCorrect) {
             return res.status(401).send(Logger.w("Validate code invalid"));
         }
@@ -137,13 +84,14 @@ router.post("/register_biometrics", (req, res, next) => {
     STORAGE.publicKeys[USER_EMAIL].push(publicKey);
 
     Logger.m("Registered biometrics for public key", publicKey);
+
     res.status(200).send(true)
 
     // res.redirect("/request_biometric_challenge");
 })
 
 router.post("/authorize_transaction", (req, res) => {
-    const {transactionID, signedChallenge, validateCode, otp} = req.body ?? {};
+    const {transactionID, signedChallenge, validateCode} = req.body ?? {};
     const validateCodes = STORAGE.validateCodes[USER_EMAIL] ?? [];
 
     if (!transactionID) {
@@ -158,25 +106,30 @@ router.post("/authorize_transaction", (req, res) => {
 
     if (signedChallenge) {
         Logger.m("Authorizing transaction", transactionID, "with signed challenge", signedChallenge);
-        const authorized = userPublicKeys.some((key) => isTokenValid(signedChallenge, key));
+
+        const authorized = userPublicKeys.some((publicKey) => isChallengeValid(signedChallenge, publicKey));
         Logger[authorized ? "m" : "w"](authorized ? "User authorized successfully using challenge" : "Unable to authorize user using challenge");
+
         return res.status(authorized ? 200 : 401).send(authorized);
     }
 
     if (validateCode) {
         Logger.m("Authorizing transaction", transactionID, "with validate code", validateCode);
+
         const isValidateCodeCorrect = validateCodes.at(-1) === validateCode;
         if (isValidateCodeCorrect) {
             validateCodes.pop();
         }
+
         Logger[isValidateCodeCorrect ? "m" : "w"](isValidateCodeCorrect ? "User authorized successfully using validate code" : "Unable to authorize user using validate code");
+
         return res.status(isValidateCodeCorrect ? 200 : 401).send(isValidateCodeCorrect);
     }
 
     res.status(400).send(Logger.w("Bad request"));
 })
 
-router.use(function(req, res, next) {
+router.use(function(req, res) {
     res.send("Error: Not Found");
 });
 
---
+++ utils.js
@@ -0,0 +1,83 @@
+const ed = require('@noble/ed25519');
+
+const {sha512} = require("@noble/hashes/sha2");
+ed.etc.sha512Sync = (...m) => sha512(ed.etc.concatBytes(...m));
+
+const USER_EMAIL = "user@example.com";
+
+const STORAGE  = {
+    publicKeys: {},
+    validateCodes: {},
+    challenges: {}
+}
+
+const DISABLE_LOGGER = false;
+
+const Logger = {
+    e: (...args) => !DISABLE_LOGGER && console.error("[ERROR]", ...args, "\n") ? args : undefined,
+    w: (...args) => !DISABLE_LOGGER && console.warn("[WARN]", ...args, "\n") ? args : undefined,
+    m: (...args) => !DISABLE_LOGGER && console.log("[INFO]", ...args, "\n") ? args : undefined,
+};
+
+function generateSixDigitNumber() {
+    return Math.floor(Math.random() * 900000) + 100000;
+}
+
+const getOriginalChallengeJWT = (signature, key) => {
+    const challengeKeys = Object.keys(STORAGE.challenges);
+
+    const challengeJWT = challengeKeys.find(challengeKey => {
+        Logger.m("Verifying signature", signature, "for nonce", challengeKey, "with key", key);
+        let verifyResult;
+        try {
+            verifyResult = ed.verify(signature, new TextEncoder().encode(challengeKey), key);
+        } catch (e) {
+            Logger.e(e);
+        }
+        Logger.m("Verification for signature", signature, "result:", verifyResult ? "success" : "failed");
+        return verifyResult;
+    })
+    if (!challengeJWT) {
+        return null;
+    }
+
+    return challengeJWT;
+}
+
+const isChallengeValid = function (signedJWTString, publicKey) {
+    try {
+        const challengeJWT = getOriginalChallengeJWT(signedJWTString, publicKey) ?? {};
+
+        if (!challengeJWT) {
+            return false;
+        }
+
+        const {nonce, expires} = STORAGE.challenges[challengeJWT];
+
+        if (!nonce || !expires) {
+            return false;
+        }
+
+        delete STORAGE.challenges[challengeJWT];
+        const challengeExpired = expires < Date.now();
+
+        if (challengeExpired) {
+            Logger.m(`Challenge ${challengeJWT} expired, removed from storage`)
+        } else {
+            Logger.m(`Challenge ${challengeJWT} success, removed from storage`)
+        }
+
+        return !challengeExpired;
+    } catch (e) {
+        return false;
+    }
+}
+
+module.exports = {
+    isChallengeValid,
+    generateSixDigitNumber,
+    STORAGE,
+    USER_EMAIL,
+    Logger,
+    ed
+}
